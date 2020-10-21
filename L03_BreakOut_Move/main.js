b847ef6f801036ba07153c60bbe6b60d1ef6eecc
Markus Damm <markus.damm@hs-Furtwangen.de>
2020-10-21 16:45:55 +0200

    Put border Blocks in place

--- L03_BreakOut_Move/main.js
+++ L03_BreakOut_Move/main.js
@@ -6,8 +6,8 @@ var L03_BreakOut_Move;
     let root = new ƒ.Node("Root");
     let ball;
     let ballRect;
-    let block;
-    let blockRect;
+    let blocks = [];
+    let blocksRects = [];
     let cmpCamera = new ƒ.ComponentCamera();
     let cameraDistance = 40;
     let viewPort;
@@ -28,10 +28,34 @@ var L03_BreakOut_Move;
         let colorBall = ƒ.Color.CSS("green");
         ball = createBall(colorBall);
         root.appendChild(ball);
-        ballRect = new ƒ.Rectangle(0, 0, 1, 1, ƒ.ORIGIN2D.CENTER);
         let border = new ƒ.Node("Border");
         root.appendChild(border);
         // 4 Blocks for the border needed
+        let colorBorder = ƒ.Color.CSS("white");
+        let borderPosition;
+        let borderScale;
+        for (let i = 0; i < 4; i++) {
+            switch (i) {
+                case 0:
+                    borderPosition = new ƒ.Vector3(0, 14);
+                    borderScale = new ƒ.Vector3(35, 1);
+                    break;
+                case 1:
+                    borderPosition = new ƒ.Vector3(17);
+                    borderScale = new ƒ.Vector3(1, 28);
+                    break;
+                case 2:
+                    borderPosition = new ƒ.Vector3(0, -14);
+                    borderScale = new ƒ.Vector3(35, 1);
+                    break;
+                case 3:
+                    borderPosition = new ƒ.Vector3(-17);
+                    borderScale = new ƒ.Vector3(1, 28);
+            }
+            let borderBlock = createBlock(colorBorder, borderPosition, borderScale);
+            // console.log("border " + i + " at " + borderPosition + " with " + borderScale);
+            border.appendChild(borderBlock);
+        }
         let colorBlock = ƒ.Color.CSS("orange");
         let blockPosition = new ƒ.Vector3(0, 10, 0);
         let block = createBlock(colorBlock, blockPosition);
@@ -62,25 +86,29 @@ var L03_BreakOut_Move;
         let v3ScaleBall = new ƒ.Vector3(1, 1, 1);
         cmpTransBall.local.scale(v3ScaleBall);
         ball.addComponent(cmpTransBall);
+        ballRect = new ƒ.Rectangle();
         return ball;
     }
-    function createBlock(_color, _position, _scale) {
-        block = new ƒ.Node("Block");
+    function createBlock(_color, _position, _scale = new ƒ.Vector3(3, 1, 1)) {
+        let block = new ƒ.Node("Block");
         let meshBlock = new ƒ.MeshCube("MeshBlock");
         let cmpMeshBlock = new ƒ.ComponentMesh(meshBlock);
         block.addComponent(cmpMeshBlock);
-        let matBlock = new ƒ.Material("BallMat", ƒ.ShaderUniColor, new ƒ.CoatColored(_color));
+        let matBlock = new ƒ.Material("BlockMat", ƒ.ShaderUniColor, new ƒ.CoatColored(_color));
         let cmpMatBlock = new ƒ.ComponentMaterial(matBlock);
         block.addComponent(cmpMatBlock);
         let cmpTransBlock = new ƒ.ComponentTransform();
-        let v3ScaleBlock = new ƒ.Vector3(3, 1, 1);
-        cmpTransBlock.local.scale(v3ScaleBlock);
+        cmpTransBlock.local.scale(_scale);
         if (_position) {
             cmpTransBlock.local.translate(_position);
         }
         block.addComponent(cmpTransBlock);
-        blockRect = new ƒ.Rectangle();
-        blockRect.position = block.cmpTransform.local.translation.toVector2();
+        let blockRect = new ƒ.Rectangle(0, 0, _scale.x, _scale.y);
+        let rectPos = new ƒ.Vector2(block.mtxLocal.translation.x - (_scale.x / 2), block.mtxLocal.translation.y - (_scale.y / 2));
+        blockRect.position = rectPos;
+        console.log(blockRect.height);
+        blocks.push(block);
+        blocksRects.push(blockRect);
         return block;
     }
     function hdlInput(_event) {
@@ -94,33 +122,26 @@ var L03_BreakOut_Move;
         let mulitplier = 2;
         ballRect.position = ball.mtxLocal.translation.toVector2();
         // console.log(ƒ.Time.game.getElapsedSincePreviousCall());
-        if (ballRect.collides(blockRect)) {
-            console.log("Ball collides with Block!");
-            let intersection = ballRect.getIntersection(blockRect);
-            if (intersection.size.x > intersection.size.y) {
-                yInput.value = (Number(yInput.value) * -1).toString();
-                document.querySelector("div").dispatchEvent(new Event("input"));
+        for (const blockRect of blocksRects) {
+            if (ballRect.collides(blockRect)) {
+                console.log("Ball collides with Block!");
+                let intersection = ballRect.getIntersection(blockRect);
+                if (intersection.size.x > intersection.size.y) {
+                    yInput.value = (Number(yInput.value) * -1).toString();
+                    document.querySelector("div").dispatchEvent(new Event("input"));
+                }
+                else if (intersection.size.x < intersection.size.y) {
+                    xInput.value = (Number(xInput.value) * -1).toString();
+                    document.querySelector("div").dispatchEvent(new Event("input"));
+                }
+                // ƒ.DebugConsole(ballRect.getIntersection(blockRect).position);
             }
-            else if (intersection.size.x < intersection.size.y) {
-                xInput.value = (Number(xInput.value) * -1).toString();
-                document.querySelector("div").dispatchEvent(new Event("input"));
-            }
-            // ƒ.DebugConsole(ballRect.getIntersection(blockRect).position);
         }
         let frameTime = ƒ.Time.game.getElapsedSincePreviousCall() / 1000;
         // für ständig neue "Wegwerf-Vectoren" kann der Recycler von Fudge genutzt werden ƒ.Recycler
         let v3Velocity = new ƒ.Vector3(xSpeed * mulitplier, ySpeed * mulitplier, 0);
         v3Velocity.scale(frameTime);
         ball.mtxLocal.translate(v3Velocity);
-        let border = cameraDistance * 0.35;
-        if (ball.mtxLocal.translation.x <= -border || ball.mtxLocal.translation.x >= border) {
-            xInput.value = (Number(xInput.value) * -1).toString();
-            document.querySelector("div").dispatchEvent(new Event("input"));
-        }
-        if (ball.mtxLocal.translation.y <= -border || ball.mtxLocal.translation.y >= border) {
-            yInput.value = (Number(yInput.value) * -1).toString();
-            document.querySelector("div").dispatchEvent(new Event("input"));
-        }
         viewPort.draw();
     }
 })(L03_BreakOut_Move || (L03_BreakOut_Move = {}));
--- L03_BreakOut_Move/main.ts
+++ L03_BreakOut_Move/main.ts
@@ -2,11 +2,13 @@ namespace L03_BreakOut_Move {
     import ƒ = FudgeCore;
 
     document.addEventListener("DOMContentLoaded", init);
+
     let root: ƒ.Node = new ƒ.Node("Root");
     let ball: ƒ.Node;
     let ballRect: ƒ.Rectangle;
-    let block: ƒ.Node;
-    let blockRect: ƒ.Rectangle;
+    let blocks: ƒ.Node[] = [];
+    let blocksRects: ƒ.Rectangle[] = [];
+
     let cmpCamera: ƒ.ComponentCamera = new ƒ.ComponentCamera();
     let cameraDistance: number = 40;
     let viewPort: ƒ.Viewport;
@@ -32,11 +34,38 @@ namespace L03_BreakOut_Move {
         let colorBall: ƒ.Color = ƒ.Color.CSS("green");
         ball = createBall(colorBall);
         root.appendChild(ball);
-        ballRect = new ƒ.Rectangle(0, 0, 1, 1, ƒ.ORIGIN2D.CENTER);
 
         let border: ƒ.Node = new ƒ.Node("Border");
         root.appendChild(border);
         // 4 Blocks for the border needed
+
+        let colorBorder: ƒ.Color = ƒ.Color.CSS("white");
+        let borderPosition: ƒ.Vector3;
+        let borderScale: ƒ.Vector3;
+        for (let i: number = 0; i < 4; i++) {
+            switch (i) {
+                case 0:
+                    borderPosition = new ƒ.Vector3(0, 14);
+                    borderScale = new ƒ.Vector3(35, 1);
+                    break;
+                case 1:
+                    borderPosition = new ƒ.Vector3(17);
+                    borderScale = new ƒ.Vector3(1, 28);
+                    break;
+                case 2:
+                    borderPosition = new ƒ.Vector3(0, -14);
+                    borderScale = new ƒ.Vector3(35, 1);
+                    break;
+                case 3:
+                    borderPosition = new ƒ.Vector3(-17);
+                    borderScale = new ƒ.Vector3(1, 28);
+            }
+
+            let borderBlock: ƒ.Node = createBlock(colorBorder, borderPosition, borderScale);
+            // console.log("border " + i + " at " + borderPosition + " with " + borderScale);
+            
+            border.appendChild(borderBlock);
+        }
 
         let colorBlock: ƒ.Color = ƒ.Color.CSS("orange");
         let blockPosition: ƒ.Vector3 = new ƒ.Vector3(0, 10, 0);
@@ -75,30 +104,36 @@ namespace L03_BreakOut_Move {
         cmpTransBall.local.scale(v3ScaleBall);
         ball.addComponent(cmpTransBall);
 
+        ballRect = new ƒ.Rectangle();
+
         return ball;
     }
 
-    function createBlock(_color: ƒ.Color, _position?: ƒ.Vector3, _scale?: ƒ.Vector3): ƒ.Node {
-        block = new ƒ.Node("Block");
+    function createBlock(_color: ƒ.Color, _position?: ƒ.Vector3, _scale: ƒ.Vector3 = new ƒ.Vector3(3, 1, 1)): ƒ.Node {
+        let block: ƒ.Node = new ƒ.Node("Block");
         let meshBlock: ƒ.MeshCube = new ƒ.MeshCube("MeshBlock");
         let cmpMeshBlock: ƒ.ComponentMesh = new ƒ.ComponentMesh(meshBlock);
         block.addComponent(cmpMeshBlock);
 
-        let matBlock: ƒ.Material = new ƒ.Material("BallMat", ƒ.ShaderUniColor, new ƒ.CoatColored(_color));
+        let matBlock: ƒ.Material = new ƒ.Material("BlockMat", ƒ.ShaderUniColor, new ƒ.CoatColored(_color));
         let cmpMatBlock: ƒ.ComponentMaterial = new ƒ.ComponentMaterial(matBlock);
         block.addComponent(cmpMatBlock);
 
         let cmpTransBlock: ƒ.ComponentTransform = new ƒ.ComponentTransform();
-        let v3ScaleBlock: ƒ.Vector3 = new ƒ.Vector3(3, 1, 1);
-        cmpTransBlock.local.scale(v3ScaleBlock);
+        cmpTransBlock.local.scale(_scale);
         if (_position) {
             cmpTransBlock.local.translate(_position);
         }
         block.addComponent(cmpTransBlock);
 
-        blockRect = new ƒ.Rectangle();
-        blockRect.position = block.cmpTransform.local.translation.toVector2();
+        let blockRect: ƒ.Rectangle = new ƒ.Rectangle(0, 0, _scale.x, _scale.y);
+        let rectPos: ƒ.Vector2 = new ƒ.Vector2(block.mtxLocal.translation.x - (_scale.x / 2), block.mtxLocal.translation.y - (_scale.y / 2));
+        blockRect.position = rectPos;
+        console.log(blockRect.height);
+        
 
+        blocks.push(block);
+        blocksRects.push(blockRect);
         return block;
     }
 
@@ -114,18 +149,20 @@ namespace L03_BreakOut_Move {
         let mulitplier: number = 2;
         ballRect.position = ball.mtxLocal.translation.toVector2();
         // console.log(ƒ.Time.game.getElapsedSincePreviousCall());
-        if (ballRect.collides(blockRect)) {
-            console.log("Ball collides with Block!");
-            let intersection: ƒ.Rectangle = ballRect.getIntersection(blockRect);
-            if (intersection.size.x > intersection.size.y) {
-                yInput.value = (Number(yInput.value) * -1).toString();
-                document.querySelector("div").dispatchEvent(new Event("input"));
+        for (const blockRect of blocksRects) {
+            if (ballRect.collides(blockRect)) {
+                console.log("Ball collides with Block!");
+                let intersection: ƒ.Rectangle = ballRect.getIntersection(blockRect);
+                if (intersection.size.x > intersection.size.y) {
+                    yInput.value = (Number(yInput.value) * -1).toString();
+                    document.querySelector("div").dispatchEvent(new Event("input"));
+                }
+                else if (intersection.size.x < intersection.size.y) {
+                    xInput.value = (Number(xInput.value) * -1).toString();
+                    document.querySelector("div").dispatchEvent(new Event("input"));
+                }
+                // ƒ.DebugConsole(ballRect.getIntersection(blockRect).position);
             }
-            else if (intersection.size.x < intersection.size.y) {
-                xInput.value = (Number(xInput.value) * -1).toString();
-                document.querySelector("div").dispatchEvent(new Event("input"));
-            }
-            // ƒ.DebugConsole(ballRect.getIntersection(blockRect).position);
         }
 
         let frameTime: number = ƒ.Time.game.getElapsedSincePreviousCall() / 1000;
@@ -134,16 +171,6 @@ namespace L03_BreakOut_Move {
         let v3Velocity: ƒ.Vector3 = new ƒ.Vector3(xSpeed * mulitplier, ySpeed * mulitplier, 0);
         v3Velocity.scale(frameTime);
         ball.mtxLocal.translate(v3Velocity);
-
-        let border: number = cameraDistance * 0.35;
-        if (ball.mtxLocal.translation.x <= -border || ball.mtxLocal.translation.x >= border) {
-            xInput.value = (Number(xInput.value) * -1).toString();
-            document.querySelector("div").dispatchEvent(new Event("input"));
-        }
-        if (ball.mtxLocal.translation.y <= -border || ball.mtxLocal.translation.y >= border) {
-            yInput.value = (Number(yInput.value) * -1).toString();
-            document.querySelector("div").dispatchEvent(new Event("input"));
-        }
 
         viewPort.draw();
     }
