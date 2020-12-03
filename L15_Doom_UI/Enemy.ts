df846b12cfe81e073070af758cdab94823678207
MarkusDamm <markus.damm@hs-furtwangen.de>
2020-12-03 14:10:26 +0100

    Add UI-PNGs

---
+++ L15_Doom_UI/Enemy.ts
@@ -0,0 +1,144 @@
+namespace L15_Doom {
+  import ƒ = FudgeCore;
+  import ƒAid = FudgeAid;
+
+  export enum ANGLE {
+    // N = 0, NE = 1, E = 2, SE = 3, S = 4, SW = 5, W = 6, NW = 7,
+    _000 = 0, _045 = 1, _090 = 2, _135 = 3, _180 = 4, _225 = 5, _270 = 6, _315 = 7
+  }
+
+  export enum STATE {
+    IDLE, STOP, MOVE, ATTACK, DIE
+  }
+
+  export class Enemy extends GameObject {
+    protected static readonly TXT_ENEMY: ƒ.TextureImage = new ƒ.TextureImage("../DoomAssets/Cyberdemon01.png");
+    protected static readonly SIZE: ƒ.Vector2 = ƒ.Vector2.ONE(2);
+
+    private static animations: ƒAid.SpriteSheetAnimations;
+
+    protected show: ƒ.Node;
+    protected sprite: ƒAid.NodeSprite;
+    protected posTarget: ƒ.Vector3;
+
+    protected health: number;
+    protected speed: number = 2;
+    protected angle: number;
+    protected angleView: number = 0;
+
+    protected state: STATE = STATE.IDLE;
+
+    constructor(_position: ƒ.Vector3) {
+      super("Enemy", _position, ƒ.Vector3.ZERO());
+
+      this.show = new ƒAid.Node("Show", ƒ.Matrix4x4.IDENTITY());
+      this.appendChild(this.show);
+
+      this.sprite = new ƒAid.NodeSprite("Sprite");
+      this.sprite.addComponent(new ƒ.ComponentTransform());
+      this.show.appendChild(this.sprite);
+
+      this.sprite.setAnimation(<ƒAid.SpriteSheetAnimation>Enemy.animations["Idle_000"]);
+      this.sprite.setFrameDirection(1);
+      this.sprite.framerate = 2;
+
+      this.chooseTargetPosition();
+      this.changeState(STATE.MOVE);
+    }
+
+    public static generateSprites(_spritesheet: ƒ.CoatTextured): void {
+      Enemy.animations = {};
+      for (let angle: number = 0; angle < 8; angle++) {
+        let name: string = "Idle" + ANGLE[angle];
+        let sprite: ƒAid.SpriteSheetAnimation = new ƒAid.SpriteSheetAnimation(name, _spritesheet);
+        sprite.generateByGrid(ƒ.Rectangle.GET(44 + angle * 130, 33, 82, 108), 4, 32, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.Y(143));
+        Enemy.animations[name] = sprite;
+      }
+    }
+
+    public update(_avatarPosition: ƒ.Vector3): void {
+      this.show.mtxLocal.showTo(ƒ.Vector3.TRANSFORMATION(_avatarPosition, this.mtxWorldInverse, true), ƒ.Vector3.Y(), true);
+      this.adjustSprite();
+
+      switch (this.state) {
+        case STATE.MOVE:
+          if (this.mtxLocal.translation.equals(this.posTarget, 0.1))
+            this.changeState(STATE.STOP);
+          else
+            this.move();
+          break;
+        case STATE.STOP:
+          this.changeState(STATE.IDLE);
+          this.sprite.framerate = 0.2;
+          setTimeout(this.changeState, 3000);
+          // console.log("Stoped; change to idle, then to Move");
+          break;
+        case STATE.IDLE:
+        default:
+          break;
+        case STATE.ATTACK:
+          // attack
+          break;
+        case STATE.DIE:
+          // change animation to dying
+          break;
+      }
+    }
+
+    protected changeState = (_state: STATE = STATE.MOVE): void => {
+      this.state = _state;
+      this.sprite.framerate = 2;
+      if (_state == STATE.MOVE) {
+        this.chooseTargetPosition();
+      }
+    }
+
+    protected move(): void {
+      this.mtxLocal.showTo(this.posTarget, ƒ.Vector3.Y(), true);
+      this.mtxLocal.translateZ(this.speed * ƒ.Loop.timeFrameGame / 1000);
+    }
+
+    protected checkVision(): boolean {
+
+      return true;
+    }
+
+    protected adjustSprite(): void {
+      let rotation: number = this.show.mtxLocal.rotation.y;
+      rotation = (rotation + 360 + 22.5) % 360;
+      rotation = Math.floor(rotation / 45);
+
+      if (this.angleView == rotation)
+        return;
+
+      this.angleView = rotation;
+
+      if (rotation > 4) {
+        rotation = 8 - rotation;
+        this.flip(true);
+      }
+      else
+        this.flip(false);
+
+      let section: string = ANGLE[rotation]; // .padStart(3, "0");
+      console.log(section);
+      this.sprite.setAnimation(<ƒAid.SpriteSheetAnimation>Enemy.animations["Idle" + section]);
+    }
+
+    protected flip(_reverse: boolean): void {
+      this.sprite.mtxLocal.rotation = ƒ.Vector3.Y(_reverse ? 180 : 0);
+    }
+
+    protected chooseTargetPosition(): void {
+      let range: number = sizeWall * numWalls / 2 - 2;
+      this.posTarget = new ƒ.Vector3(ƒ.Random.default.getRange(-range, range), 0, ƒ.Random.default.getRange(-range, range));
+    }
+
+    // bis Donnerstag: Gegner bei Sichtkontakt zum Spieler auf ihn zu bewegen
+    // -> Ray zum Spieler -> wenn der Ray eine Wand zw Figur und Spieler trifft
+    // Interface vorbereiten (HTML-Overlay für die meisten Elemente?) Sprite fürs Doom-Guy-Gesicht
+
+    // 17.12. Besprechung der Spielkonzepte
+  }
+
+}
---
+++ L15_Doom_UI/Main.ts
@@ -0,0 +1,140 @@
+namespace L15_Doom {
+  import ƒ = FudgeCore;
+  import ƒaid = FudgeAid;
+
+  window.addEventListener("load", hndLoad);
+
+  export let viewport: ƒ.Viewport;
+  let root: ƒ.Node = new ƒ.Node("Root");
+  let avatar: Avatar;
+  let enemies: ƒ.Node;
+
+  export let walls: ƒ.Node;
+  export const sizeWall: number = 3;
+  export const numWalls: number = 20;
+
+  const clrWhite: ƒ.Color = ƒ.Color.CSS("white");
+
+  async function hndLoad(_event: Event): Promise<void> {
+    const canvas: HTMLCanvasElement = document.querySelector("canvas");
+
+    let floor: ƒaid.Node = createFloor();
+    root.appendChild(floor);
+
+    walls = createWalls();
+    root.appendChild(walls);
+
+    enemies = await createEnemies();
+    root.appendChild(enemies);
+
+    avatar = new Avatar();
+    root.appendChild(avatar);
+
+    viewport = new ƒ.Viewport();
+    viewport.initialize("Viewport", root, avatar.getComponent(ƒ.ComponentCamera), canvas);
+    viewport.draw();
+
+    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, hndLoop);
+    ƒ.Loop.start(ƒ.LOOP_MODE.TIME_GAME, 60);
+
+    canvas.addEventListener("mousemove", hndMouseMove);
+    canvas.addEventListener("click", canvas.requestPointerLock);
+  }
+
+  async function createEnemies(): Promise<ƒ.Node> {
+    let enemies: ƒ.Node = new ƒ.Node("Enemies");
+
+    let txtEnemy: ƒ.TextureImage = new ƒ.TextureImage();
+    await txtEnemy.load("../DoomAssets/Cyberdemon_transparent.png");
+    let coatSprite: ƒ.CoatTextured = new ƒ.CoatTextured(clrWhite, txtEnemy);
+    Enemy.generateSprites(coatSprite);
+
+    enemies.appendChild(new Enemy(new ƒ.Vector3(4, 0, -3)));
+    enemies.appendChild(new Enemy(new ƒ.Vector3(-4, 0, -3)));
+
+    return enemies;
+  }
+
+  function createFloor(): ƒaid.Node {
+    let meshQuad: ƒ.MeshQuad = new ƒ.MeshQuad("Quad");
+
+    let txtFloor: ƒ.TextureImage = new ƒ.TextureImage("../DoomAssets/DEM1_5.png");
+    let mtrFloor: ƒ.Material = new ƒ.Material("Floor", ƒ.ShaderTexture, new ƒ.CoatTextured(clrWhite, txtFloor));
+    let floor: ƒaid.Node = new ƒaid.Node("Floor", ƒ.Matrix4x4.ROTATION_X(-90), mtrFloor, meshQuad);
+    floor.mtxLocal.scale(ƒ.Vector3.ONE(sizeWall * numWalls));
+    floor.getComponent(ƒ.ComponentMaterial).pivot.scale(ƒ.Vector2.ONE(numWalls));
+    return floor;
+  }
+
+  function hndLoop(_event: Event): void {
+    avatar.update();
+    for (let enemy of enemies.getChildren() as Enemy[]) {
+      // enemy.update();
+      enemy.update(avatar.mtxWorld.translation);
+    }
+    viewport.draw();
+  }
+
+  function hndMouseMove(_event: MouseEvent): void {
+    // console.log(_event.movementX, _event.movementY);
+    avatar.rotate(_event);
+  }
+
+  function createWalls(): ƒ.Node {
+    let walls: ƒ.Node = new ƒ.Node("Walls");
+
+    let txtWall: ƒ.TextureImage = new ƒ.TextureImage("../DoomAssets/CEMPOIS.png");
+    let mtrWall: ƒ.Material = new ƒ.Material("Wall", ƒ.ShaderTexture, new ƒ.CoatTextured(clrWhite, txtWall));
+
+    // walls.appendChild(new Wall(ƒ.Vector2.ONE(3), ƒ.Vector3.Y(sizeWall / 2), ƒ.Vector3.ZERO(), mtrWall));
+    // walls.appendChild(new Wall(ƒ.Vector2.ONE(3), ƒ.Vector3.SCALE(new ƒ.Vector3(0.5, 1, -0.866), sizeWall / 2), ƒ.Vector3.Y(120), mtrWall));
+    // walls.appendChild(new Wall(ƒ.Vector2.ONE(3), ƒ.Vector3.SCALE(new ƒ.Vector3(-0.5, 1, -0.866), sizeWall / 2), ƒ.Vector3.Y(-120), mtrWall));
+
+    for (let i: number = -numWalls / 2 + 0.5; i < numWalls / 2; i++) {
+      walls.appendChild(new Wall(ƒ.Vector2.ONE(3), ƒ.Vector3.SCALE(new ƒ.Vector3(-numWalls / 2, 0.5, i), sizeWall), ƒ.Vector3.Y(90), mtrWall));
+
+      // for (let i: number = -numWalls / 2 + 0.5; i < numWalls / 2; i++)
+      walls.appendChild(new Wall(ƒ.Vector2.ONE(3), ƒ.Vector3.SCALE(new ƒ.Vector3(numWalls / 2, 0.5, i), sizeWall), ƒ.Vector3.Y(-90), mtrWall));
+
+      // for (let i: number = -numWalls / 2 + 0.5; i < numWalls / 2; i++)
+      walls.appendChild(new Wall(ƒ.Vector2.ONE(3), ƒ.Vector3.SCALE(new ƒ.Vector3(i, 0.5, -numWalls / 2), sizeWall), ƒ.Vector3.Y(0), mtrWall));
+
+      // for (let i: number = -numWalls / 2 + 0.5; i < numWalls / 2; i++)
+      walls.appendChild(new Wall(ƒ.Vector2.ONE(3), ƒ.Vector3.SCALE(new ƒ.Vector3(i, 0.5, numWalls / 2), sizeWall), ƒ.Vector3.Y(180), mtrWall));
+    }
+
+    return walls;
+  }
+
+  export function bounceOffWalls(_walls: Wall[]): Wall[] {
+    let bouncedOff: Wall[] = [];
+    let posAvatar: ƒ.Vector3 = avatar.mtxLocal.translation;
+
+    for (let wall of _walls) {
+      let posBounce: ƒ.Vector3 = wall.calculateBounce(posAvatar, 1);
+      if (posBounce) {
+        avatar.mtxLocal.translation = posBounce;
+        bouncedOff.push(wall);
+      }
+    }
+    return bouncedOff;
+  }
+
+  export function calculateAngle(_vectorA: ƒ.Vector3, _vectorB: ƒ.Vector3): number {
+    let angle: number = 0;
+    let dotProduct: number = ƒ.Vector3.DOT(_vectorA, _vectorB);
+    let lengthA: number = calculateVectorLenght(_vectorA);
+    let lengthB: number = calculateVectorLenght(_vectorB);
+    angle = dotProduct / (lengthA * lengthB);
+    console.log(_vectorA);
+
+    return angle;
+  }
+
+  function calculateVectorLenght(_vector: ƒ.Vector3): number {
+    let length: number = _vector.x * _vector.x + _vector.y * _vector.y + _vector.z * _vector.z;
+    length = Math.sqrt(length);
+    return length;
+  }
+
+}
